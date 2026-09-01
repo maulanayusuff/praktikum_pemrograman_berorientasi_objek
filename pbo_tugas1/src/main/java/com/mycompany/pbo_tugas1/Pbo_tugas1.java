c497376db0f2bc055cb1c5e72ad9971fa9f0dc54
Maulana Yusuf <maulanayusuff@students.untidar.ac.id>
2026-09-01 21:53:06 +0700

    Add files via upload

---
+++ pbo_tugas1/src/main/java/com/mycompany/pbo_tugas1/Pbo_tugas1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo_tugas1;
+
+/**
+ *
+ * @author yusuf
+ */
+public class Pbo_tugas1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Maulana Yusuf");
+        System.out.println("NPM: 2505060038");
+        System.out.println("Alamat: Yogyakarta");
+        System.out.println("No hp: 0987654321");
+    }
+}
